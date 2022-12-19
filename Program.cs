de93a96722fa4b487f2e48fc284f308792fe3b02
dmitry <sdmitry>
2022-12-19 19:18:28 +0500

    pervay zadacha

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("введите 2 числа");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a==b)
+    Console.WriteLine($"{a}={b}");
+if (a>b)
+    Console.WriteLine($"{a} больше {b}");
+if (a<b)
+{
+    Console.WriteLine($"{b} больше {a}");
+}
